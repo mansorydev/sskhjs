b8cff7115e22f785df28937429512cdf2ce9aa00
MSD <mansory.php@gmail.com>
2021-05-24 16:22:14 +0430

    commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,60 @@
 <template>
   <v-app id="bg-pic" :style="{ backgroundImage: `url(${theme})` }">
+    <v-navigation-drawer v-if="mobile" v-model="drawer" right app>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>سولار صنعت خراسان</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          nuxt
+          :to="{ name: item.link }"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar clipped-right flat app dense>
-      <v-app-bar-nav-icon v-if="mobile"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        v-if="mobile"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
 
       <v-toolbar-title class="">سولار صنعت خراسان</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon nuxt to="/">
+      <v-btn v-if="!mobile" icon nuxt to="/">
         <span>خانه</span>
       </v-btn>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon nuxt :to="{ name: 'articles' }">
+      <v-btn v-if="!mobile" icon nuxt :to="{ name: 'articles' }">
         <span>مقالات</span>
       </v-btn>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon nuxt :to="{ name: 'aboutus' }">
+      <v-btn v-if="!mobile" icon nuxt :to="{ name: 'aboutus' }">
         <span>درباره ما</span>
       </v-btn>
 
@@ -59,6 +94,7 @@
         </v-card>
       </v-menu>
     </v-app-bar>
+
     <v-main>
       <nuxt />
     </v-main>
@@ -72,7 +108,13 @@
 export default {
   data() {
     return {
+      drawer: false,
       name: 'انتخاب قالب ...',
+      items: [
+        { link: 'index', title: 'خانه', icon: 'mdi-home' },
+        { link: 'articles', title: 'مقالات', icon: 'mdi-forum' },
+        { link: 'aboutus', title: 'درباره ما', icon: 'mdi-forum' },
+      ],
       pics: [
         { link: require(`~/assets/images/theme/1.jpg`) },
         { link: require(`~/assets/images/theme/2.jpg`) },
