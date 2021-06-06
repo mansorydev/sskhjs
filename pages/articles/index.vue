3322a4d91f6a5e9564f31bda646c2a3ae9fcbe08
MSD <mansory.php@gmail.com>
2021-06-07 00:23:48 +0430

    commit

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,18 +1,15 @@
 <template>
   <v-container>
     <v-row class="pt-16">
-      <v-col class="col-12 col-md-4">
+      <v-col v-for="(post, i) of posts" :key="i" class="col-12 col-md-4">
         <v-card class="mx-auto" max-width="344" outlined>
           <v-list-item three-line>
             <v-list-item-content>
-              <div class="overline mb-4">لورم ایپسوم</div>
+              <div class="overline mb-4">{{ post.title }}</div>
               <v-list-item-title class="headline mb-1">
                 تکنولوژی
               </v-list-item-title>
-              <v-list-item-subtitle
-                >لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                استفاده از طراحان گرافیک است.</v-list-item-subtitle
-              >
+              <v-list-item-subtitle>{{ post.body }}</v-list-item-subtitle>
             </v-list-item-content>
 
             <v-list-item-avatar
@@ -23,124 +20,8 @@
           </v-list-item>
 
           <v-card-actions>
-            <v-btn outlined rounded text :to="{ path: `/articles/1` }">
-              کلیک
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-
-      <v-col class="col-12 col-md-4">
-        <v-card class="mx-auto" max-width="344" outlined>
-          <v-list-item three-line>
-            <v-list-item-content>
-              <div class="overline mb-4">لورم ایپسوم</div>
-              <v-list-item-title class="headline mb-1">
-                تکنولوژی
-              </v-list-item-title>
-              <v-list-item-subtitle
-                >لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                استفاده از طراحان گرافیک است.</v-list-item-subtitle
-              >
-            </v-list-item-content>
-
-            <v-list-item-avatar
-              tile
-              size="80"
-              color="grey"
-            ></v-list-item-avatar>
-          </v-list-item>
-
-          <v-card-actions>
-            <v-btn outlined rounded text :to="{ path: `/articles/1` }">
-              کلیک
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-
-      <v-col class="col-12 col-md-4">
-        <v-card class="mx-auto" max-width="344" outlined>
-          <v-list-item three-line>
-            <v-list-item-content>
-              <div class="overline mb-4">لورم ایپسوم</div>
-              <v-list-item-title class="headline mb-1">
-                تکنولوژی
-              </v-list-item-title>
-              <v-list-item-subtitle
-                >لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                استفاده از طراحان گرافیک است.</v-list-item-subtitle
-              >
-            </v-list-item-content>
-
-            <v-list-item-avatar
-              tile
-              size="80"
-              color="grey"
-            ></v-list-item-avatar>
-          </v-list-item>
-
-          <v-card-actions>
-            <v-btn outlined rounded text :to="{ path: `/articles/1` }">
-              کلیک
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-
-      <v-col class="col-12 col-md-4">
-        <v-card class="mx-auto" max-width="344" outlined>
-          <v-list-item three-line>
-            <v-list-item-content>
-              <div class="overline mb-4">لورم ایپسوم</div>
-              <v-list-item-title class="headline mb-1">
-                تکنولوژی
-              </v-list-item-title>
-              <v-list-item-subtitle
-                >لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                استفاده از طراحان گرافیک است.</v-list-item-subtitle
-              >
-            </v-list-item-content>
-
-            <v-list-item-avatar
-              tile
-              size="80"
-              color="grey"
-            ></v-list-item-avatar>
-          </v-list-item>
-
-          <v-card-actions>
-            <v-btn outlined rounded text :to="{ path: `/articles/1` }">
-              کلیک
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-
-      <v-col class="col-12 col-md-4">
-        <v-card class="mx-auto" max-width="344" outlined>
-          <v-list-item three-line>
-            <v-list-item-content>
-              <div class="overline mb-4">لورم ایپسوم</div>
-              <v-list-item-title class="headline mb-1">
-                تکنولوژی
-              </v-list-item-title>
-              <v-list-item-subtitle
-                >لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                استفاده از طراحان گرافیک است.</v-list-item-subtitle
-              >
-            </v-list-item-content>
-
-            <v-list-item-avatar
-              tile
-              size="80"
-              color="grey"
-            ></v-list-item-avatar>
-          </v-list-item>
-
-          <v-card-actions>
-            <v-btn outlined rounded text :to="{ path: `/articles/1` }">
-              کلیک
+            <v-btn outlined rounded text :to="{ path: `/articles/${post.id}` }">
+              نمایش
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -151,6 +32,14 @@
 
 <script>
 export default {
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  async fetch() {
+    this.posts = await fetch('/api/posts')
+  },
   head: {
     titleTemplate: '%s | صفحه مقالات',
   },
